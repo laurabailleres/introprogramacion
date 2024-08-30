2d6bcb1cba86bdade5355e679e74b9b1186c9f53
laura bailleres <88726253+laurabailleres@users.noreply.github.com>
2024-08-30 00:17:09 -0300

    Update simulacroParcialHaskell.hs

--- simulacroParcialHaskell.hs
+++ simulacroParcialHaskell.hs
@@ -1,3 +1,15 @@
+--POR AHORA EXACTAS FUNCIONA, PERO LOS PROBLEMAS SIGUEN!
+--GRILLA SALARIAL DOCENTE: https://aduba.org.ar/wp-content/uploads/2024/07/Instructivo-Liquidacion-Salarios-JULIO-2024.pdf
+
+
+
+
+
+
+
+
+
+
 --simulacro parcial 18 de septiembre 2023
 
 --ejercicio 1
